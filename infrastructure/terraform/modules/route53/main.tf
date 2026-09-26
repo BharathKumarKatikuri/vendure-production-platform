f2622adea394e784368ae1b0c9679effc7b20f94
Bharath Kumar Katikuri <katikuribharath@gmail.com>
2026-09-26 09:41:59 +0000

    fix: prepare Route53 hosted zone migration

--- infrastructure/terraform/modules/route53/main.tf
+++ infrastructure/terraform/modules/route53/main.tf
@@ -1,38 +1,3 @@
 resource "aws_route53_zone" "this" {
   name = var.domain_name
 }
-
-resource "aws_acm_certificate" "api" {
-  domain_name       = var.api_domain_name
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "api_certificate_validation" {
-  count = 1
-
-  zone_id = aws_route53_zone.this.zone_id
-  name    = tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_name
-  type    = tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_type
-  ttl     = 300
-  records = [
-    tolist(aws_acm_certificate.api.domain_validation_options)[0].resource_record_value
-  ]
-
-  allow_overwrite = true
-}
-
-resource "aws_route53_record" "api" {
-  zone_id = aws_route53_zone.this.zone_id
-  name    = var.api_domain_name
-  type    = "A"
-
-  alias {
-    name                   = var.alb_dns_name
-    zone_id                = var.alb_zone_id
-    evaluate_target_health = true
-  }
-}
--- infrastructure/terraform/modules/route53/outputs.tf
+++ infrastructure/terraform/modules/route53/outputs.tf
@@ -7,13 +7,3 @@ output "name_servers" {
   description = "Route 53 name servers for domain delegation."
   value       = aws_route53_zone.this.name_servers
 }
-
-output "acm_certificate_arn" {
-  description = "ARN of the ACM certificate for the API domain."
-  value       = aws_acm_certificate.api.arn
-}
-
-output "acm_certificate_domain" {
-  description = "Domain covered by the ACM certificate."
-  value       = aws_acm_certificate.api.domain_name
-}
